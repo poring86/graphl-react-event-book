e54a2ead490487b153f514c0ba47dbc26bac9208
Matheus Lino <matheuslino86a@gmail.com>
2021-05-04 19:57:35 -0400

    added bookings

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -9,6 +9,13 @@ type Event{
     date: String!
     creator: User!
 }
+type Booking{
+    _id: ID!
+    event: Event!
+    user: User!
+    createdAt: String!
+    updatedAt: String!
+}
 type User{
     _id: ID!
     email: String!
@@ -27,10 +34,13 @@ input UserInput{
 }
 type RootQuery {
     events: [Event!]!
+    bookings: [Booking!]!
 }
 type RootMutation {
     createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput): User
+    bookEvent(eventId: ID!): Booking!
+    cancelBooking(bookingId: ID!): Event!
 }
 schema {
     query: RootQuery
